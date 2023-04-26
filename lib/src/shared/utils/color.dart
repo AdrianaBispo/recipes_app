773b63aeaf929c15c5e38e1e9cf5c5b399dd041b
dev <adriana.bispo283@gmail.com>
2023-04-25 21:31:53 -0300

    :recycle: refactor: class retatorada para singleton

--- lib/src/shared/utils/color.dart
+++ lib/src/shared/utils/color.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-class AppColor{
-  static const primaria = Color(0xFFFFC439);
-  static const secundaria = Color(0xffFFFFFF);
-  static const black = Color(0xFF000000);
-  static const unselected = Color(0xFFE4E4E4);
+
+class AppColor {
+  static AppColor? _instance;
+
+  AppColor._();
+  
+  static AppColor get instance {
+    _instance ??= AppColor._();
+    return _instance!;
+  }
+
+  Color get primaria => const Color(0xFFFFC439);
+  Color get secundaria => const Color(0xffFFFFFF);
+  Color get black => const Color(0xFF000000);
+  Color get unselected => const Color(0xFFE4E4E4);
 }
